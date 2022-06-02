661088826d58043eb0b74a666f3e1a966262ac95
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-02 14:04:05 +0300

    create a view function for the index page.

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  '''
+  View function to Display the index page and its data."
+  '''
+  return render(request, 'index.html')
