b68fc5cd2d139a8ea0c52b47ebcbee2e52f2a870
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-02 14:54:14 +0300

    create a category model.

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Category(models.Model):
+  '''
+  Class to help create instances of different Photo category models."
+  '''
+  name = CharField(max_length=30)
+
+  def __str__(self):
+        return self.name
+
+  def saveCategory(self):
+      self.save()
