5ca85dce5ec4c7d9f8168ab26af7c13a6bad17d7
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-02 16:10:20 +0300

    create a search form and defined the url pattern to display the search results.

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 urlpatterns = [
   path('', views.index, name='index' ),
   path('gallery/', views.gallery, name='gallery'),
+  path('search/', views.search_results, name='search_results')
+
 ]
 
 if settings.DEBUG:
--- photos/views.py
+++ photos/views.py
@@ -11,7 +11,7 @@ def index(request):
   return render(request, 'index.html')
 
 def gallery(request):
-  images = Image.get_images()
+  images = Image.getImages()
   locations = Location.objects.all()
   categories = Category.objects.all()
   return render(request, 'gallery.html', {"images":images,"locations":locations, "categories":categories })
