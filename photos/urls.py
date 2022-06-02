9141280b157d578b819ee03668175918d7482661
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-02 15:48:05 +0300

    configure app to display uploaded images.

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path,re_path
 from . import views 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-  path('', views.index, name='index' )
+  path('', views.index, name='index' ),
+  path('gallery/', views.gallery, name='gallery'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
+from .models import Image,Category,Location
+
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,10 @@ def index(request):
   View function to Display the index page and its data."
   '''
   return render(request, 'index.html')
+
+def gallery(request):
+  images = Image.get_images()
+  locations = Location.objects.all()
+  categories = Category.objects.all()
+  return render(request, 'gallery.html', {"images":images,"locations":locations, "categories":categories })
+
