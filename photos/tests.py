4fc6538cc71a6c9bcd82ce321b66d44dd84b6004
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-02 15:17:45 +0300

    create tests for the Category Model and its methods.

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Category, Location, Image
 
 # Create your tests here.
+
+class CategoryTestclass(TestCase):
+
+  def setUp(self):
+    self.art = Category(name = 'art')
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.art,Category))
+
+  def test_save_method(self):
+    self.art.saveCategory()
+    categories = Category.objects.all()
+    self.assertTrue(len(categories) > 0)
+
+
+
